0304599d84cc18edefb0496cca9c727e26426075
freeyujuan <1484083770@qq.com>
2016-04-28 17:40:38 +0800

    1.5.1

--- text1/src/com/zsyun/test.java
+++ text1/src/com/zsyun/test.java
@@ -3,7 +3,8 @@ package com.zsyun;
 public class test {
 	public static void main(String[] args) {
 		System.out.println("版本一");
-		System.out.println("版本二");
+		String aa="bb";
+		System.out.println("版本二"+aa);
 	}
 
 }
