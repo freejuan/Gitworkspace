18623743d4810858d3219ccb58e27e2575355d33
freeyujuan <1484083770@qq.com>
2016-05-10 11:44:38 +0800

    fghfg

--- text2/src/com/zsyun/tt1.java
+++ text2/src/com/zsyun/tt1.java
@@ -2,8 +2,8 @@ package com.zsyun;
 
 public class tt1 {
 	public static void main(String[] args) {
-		System.out.println("master,master");
-		System.out.println("free1,free1");
+		System.out.println("master,master1234free1");
+		System.out.println("free1,free11234free1");
 	}
 
 }
