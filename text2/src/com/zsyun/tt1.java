f78433b40606e1664f9dc3cc3d4f0a8bbab630f0
freeyujuan <1484083770@qq.com>
2016-05-10 13:27:01 +0800

    jkl

--- text2/src/com/zsyun/tt1.java
+++ text2/src/com/zsyun/tt1.java
@@ -2,12 +2,8 @@ package com.zsyun;
 
 public class tt1 {
 	public static void main(String[] args) {
-
-
 		System.out.println("master,master1234freeqq");
 		System.out.println("free1,free11234freeqq");
-
-
 	}
 
 }
