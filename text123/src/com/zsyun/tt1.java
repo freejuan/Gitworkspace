ada490e3803d91bbc2eee5c32fa0eaae23858894
freeyujuan <1484083770@qq.com>
2016-05-11 09:19:55 +0800

    123

--- text123/src/com/zsyun/tt1.java
+++ text123/src/com/zsyun/tt1.java
@@ -3,6 +3,10 @@ package com.zsyun;
 public class tt1 {
 	public static void main(String[] args) {
 		System.out.println("master test123");
+		System.out.println("");
+	}
+	public void ss1(){
+		System.out.println("ss1");
 	}
 
 }
